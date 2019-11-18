eede6030143ba0645e5c01ce0cf5e3a0bb171327
ManasMahapatra <b316009@iiit-bh.ac.in>
2019-11-19 00:43:34 +0530

    add:WINDOWS_EVENTS

--- src/App.js
+++ src/App.js
@@ -2,17 +2,36 @@ import React from 'react';
 import BackGround from './components/background/background.component';
 import TitleType from './components/title/title.component';
 import './App.css';
-
-const App = () => {
-  return (
-    <div className="app">
-      {/* set the background */}
-      <BackGround />
-      {/* the header text */}
-      <TitleType />
-    </div>
-    
-  )
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        x : 0
+    }
+    this.onMouseMove = this.onMouseMove.bind(this);
+  }
+  componentDidMount(){
+      //set the initial state to 50% of the screen
+      this.setState({ x : (window.innerWidth) / 2 });
+  }
+  onMouseMove(event){
+    //event.Native event gives statistics in regard to the element that fired the event
+    //but we need statistics with respect to the whole window object.
+    this.setState({ x : window.event.screenX});
+  }
+  render(){
+    let x = this.state.x;
+    //setting the clippath dynamically {creating string literal}
+    let clipPathX = "polygon(0% 0%, 0% 100%, " + x + "px " + "100%, " + x + "px 0%)";
+    return(
+      <div className="app" onMouseMove={this.onMouseMove}>
+        {/* set the background */}
+        <BackGround clipPath={clipPathX}/>
+        {/* the header text */}
+        <TitleType />
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/background/background.component.jsx
+++ src/components/background/background.component.jsx
@@ -2,33 +2,12 @@ import React from 'react';
 import './background.styles.scss';
 
 //let backGroundImage = "https://images.unsplash.com/photo-1550547660-d9450f859349?ixlib=rb-1.2.1&auto=format&fit=crop&w=1301&q=80";
-class BackGround extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            x : 0
-        }
-        this.onMouseMove = this.onMouseMove.bind(this);
-    }
-    componentDidMount(){
-        //set the initial state to 50% of the screen
-        this.setState({ x : (window.innerWidth) / 2 });
-    }
-    onMouseMove(event){
-        this.setState({ x : event.nativeEvent.offsetX});
-    }
-    render(){
-        let x = this.state.x;
-        //setting the clippath dynamically {creating string literal}
-        let clipPathX = "polygon(0% 0%, 0% 100%, " + x + "px " + "100%, " + x + "px 0%)";
-        return(
-            <div className="back-ground" onMouseMove={this.onMouseMove}>
-                <div className="back-ground__under" onMouseMove={this.onMouseMove}></div>
-                {/* <h1 >x: {widthX}</h1> */}
-                <div className="back-ground__over" style={{clipPath:clipPathX}} ></div>
-            </div>
-        )
-    }
-}
+const BackGround = props => (
+    <div className="back-ground">
+        <div className="back-ground__under"></div>
+        {/* <h1 >x: {widthX}</h1> */}
+        <div className="back-ground__over" style={{clipPath:props.clipPath}} ></div>
+    </div>
+)
 
 export default BackGround;
