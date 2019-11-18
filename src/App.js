57289e393366133ec9bc45752363427dacd48a79
ManasMahapatra <b316009@iiit-bh.ac.in>
2019-11-19 00:19:42 +0530

    adding: LOGO

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import BackGround from './components/background/background.component';
+import TitleType from './components/title/title.component';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* set the background */}
+      <BackGround />
+      {/* the header text */}
+      <TitleType />
     </div>
-  );
+    
+  )
 }
 
 export default App;
---
+++ src/components/title/title.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../../assets/logo.png'
+import './title.styles.scss';
+const TitleType = () => (
+    <div className="title-type">
+        <img src={logo} alt="" className="title-type__title"/>
+    </div>
+)
+
+export default TitleType;
