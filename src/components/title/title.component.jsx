5f4480766284ab26f4ea7c995e0cb8bce9ddb156
ManasMahapatra <b316009@iiit-bh.ac.in>
2019-11-19 12:49:28 +0530

    add:LABEL

--- src/components/title/title.component.jsx
+++ src/components/title/title.component.jsx
@@ -3,6 +3,7 @@ import logo from '../../assets/logo.png'
 import './title.styles.scss';
 const TitleType = () => (
     <div className="title-type">
+        <h1 className="title-type__label">It takes two hands <br /> to hold a whoo...ooper!</h1>
         <img src={logo} alt="" className="title-type__title"/>
     </div>
 )
